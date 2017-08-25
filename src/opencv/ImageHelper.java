2650c1e29f7b55892d6cb657218ab02f5dffc242
Tobias Schaber <tobias.schaber01@metro.de>
2017-08-25 11:22:46 +0200

    clean up and extract window size

--- src/opencv/ImageHelper.java
+++ src/opencv/ImageHelper.java
@@ -20,7 +20,7 @@ public class ImageHelper {
 
     public ImageHelper() {
         panel.setLayout(new FlowLayout());
-        frame.setSize(800, 600);
+        frame.setSize(Start.WINDOW_WIDTH, Start.WINDOW_HEIGHT);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.add(scrollPane);
     }
--- src/opencv/Start.java
+++ src/opencv/Start.java
@@ -11,8 +11,11 @@ import org.opencv.imgproc.Imgproc;
 public class Start {
 
     /* change this path to an image on your disk which you want to work with */
-    public static final String imagePath = "/home/tobias/work/computervision/input.png";
+    public static final String IMAGE_PATH = "/home/tobias/work/computervision/input.png";
 
+    /* window size */
+    public static final int WINDOW_HEIGHT = 800;
+    public static final int WINDOW_WIDTH = 1600;
 
     public static void main(String[] args) {
 
@@ -22,12 +25,13 @@ public class Start {
         ImageHelper helper = new ImageHelper();
 
         /* read an image to work with from disk */
-        Mat input = Imgcodecs.imread(imagePath);
+        Mat input = Imgcodecs.imread(IMAGE_PATH);
 
         /* perform image processing on this image */
         Mat processedImage = processImage(input);
 
-        /* add the image to the panel and show the window */
+        /* add the original and the processed image to the panel and show the window */
+        helper.addImage(input);
         helper.addImage(processedImage);
 
     }
@@ -40,15 +44,17 @@ public class Start {
      */
     public static Mat processImage(Mat input) {
 
+        Mat processed = new Mat();
+
         /* * * * * * * * * * * * * * * * * * * * * * * * * * * *
          *  This is your place to start.
          *  Do whatever you want with OpenCV here!
          *  For example: Convert colors to gray
          * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-        Imgproc.cvtColor(input, input, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.cvtColor(input, processed, Imgproc.COLOR_BGR2GRAY);
 
 
-        return input;
+        return processed;
     }
 }
